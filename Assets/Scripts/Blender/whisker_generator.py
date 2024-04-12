66f8e68bf7ba8549e17533f02d12fd6f5bcc51f7
PlsBeHumble <97078554+Geo-Li@users.noreply.github.com>
2024-04-11 21:12:34 -0500

    Update code to include transformations

--- Assets/Scripts/Blender/whisker_generator.py
+++ Assets/Scripts/Blender/whisker_generator.py
@@ -2,10 +2,11 @@ import bpy
 import math
 import mathutils
 import csv
+from configparser import ConfigParser
 
-
-FILE_PATH = "/home/guru/Desktop/whiskitphysics/code/data/whisker_param_average_rat/"
-
+config = ConfigParser()
+config.read('config.ini')
+FILE_PATH = config['Data']['path']
 
 def read_csv_string(file_path):
     data = list()
@@ -43,10 +44,10 @@ def data_reader():
     whisker_geom = read_csv_float(FILE_PATH+"param_s_a.csv")
     whisker_angles = read_csv_float(FILE_PATH+"param_angles.csv")
     whisker_pos = read_csv_int(FILE_PATH+"param_side_row_col.csv")
-    base_pos = read_csv_float(FILE_PATH+"param_bp_pos.csv")
-    base_rot = read_csv_float(FILE_PATH+"param_bp_angles.csv")
+    whisker_bp_coor = read_csv_float(FILE_PATH+"param_bp_pos.csv")
+    whisker_bp_angles = read_csv_float(FILE_PATH+"param_bp_angles.csv")
     # print(whisker_angles)
-    return [whisker_names, whisker_geom, whisker_angles, whisker_pos, base_pos, base_rot]
+    return [whisker_names, whisker_geom, whisker_angles, whisker_pos, whisker_bp_coor, whisker_bp_angles]
 
 
 """
@@ -71,9 +72,27 @@ def calc_slope(length, base_radius, row, col):
     return slope
 
 
-def set_frame(obj, origin, rotation):
-    obj.location = mathutils.Vector(origin)
-    obj.rotation_euler = mathutils.Euler((rotation[0], rotation[1], rotation[2]), 'XYZ')
+def rotX(angle):
+    rot = mathutils.Quaternion((1,0,0), angle)
+    return rot.to_matrix().to_4x4()
+
+
+def rotY(angle):
+    rot = mathutils.Quaternion((0,1,0), angle)
+    return rot.to_matrix().to_4x4()
+
+
+def rotZ(angle):
+    rot = mathutils.Quaternion((0,0,1), angle)
+    return rot.to_matrix().to_4x4()
+
+
+def create_frame(origin, rotation=(0,0,0)):
+    origin_vec = mathutils.Vector(origin)
+    rotation_euler = mathutils.Euler((rotation[2], rotation[1], rotation[0]), "ZYX")
+    mat_loc = mathutils.Matrix.Translation(origin_vec)
+    mat_rot = rotation_euler.to_matrix().to_4x4()
+    return mat_loc @ mat_rot
 
 
 def create_whisker_segment(whisker_name,
@@ -85,55 +104,66 @@ def create_whisker_segment(whisker_name,
                            radius_slope,
                            radius_tip,
                            link_angles,
-                           location,
-                           rotation,
+                           base_pos,
+                           base_rot,
                            NUM_LINKS):
     
     bpy.ops.object.select_all(action='DESELECT')
     
-    prev_end_location = mathutils.Vector(location)
+    # prev_end_location = mathutils.Vector(location)
     
-    parent_obj = None
+    link_prev = None
+    
+    # parent_obj = None
     link_length = whisker_length / NUM_LINKS
     for i in range(NUM_LINKS):
         # Calculate segment size and position
         # radius = radius_base - (i * (link_length * radius_slope))
+        angle = link_angles[i]
+        total_transform = mathutils.Matrix.Identity(4)
         
-        if i > 0:
+        if i == 0:
+            rot_transform = rotZ(base_rot[0]) @ rotY(base_rot[1]) @ rotX(base_rot[2])
+            trans_transform = create_frame((link_length/2, 0, 0))
+            total_transform = link_prev @ rot_transform @ trans_transform
+        else:
             rotation = (0, 0, link_angles[i])
+            link_transform1 = create_frame((link_length/2, 0, 0))
+            link_transform2 = create_frame((0,0,0), (0, 0, angle))
+            link_transform3 = create_frame((link_length/2, 0, 0))
+            total_transform = link_prev @ link_transform1 @ link_transform2 @ link_transform3
+                    
+        # if i > 0:
+        #     offset = mathutils.Vector((0,0,link_length))
+        #     offset.rotate(mathutils.Euler(rotation))
+        #     prev_end_location += offset
             
-#        angle = link_angles[i]
-        
-        if i > 0:
-            offset = mathutils.Vector((0,0,link_length))
-            offset.rotate(mathutils.Euler(rotation))
-            prev_end_location += offset
-            
-        
-#        location = (location[0], location[1], location[2] + (link_length if i > 0 else 0))
-#        rotation = (angle,0,0)
 
         # Create cylinder
         bpy.ops.mesh.primitive_cone_add(radius1=radius_base - (i * (link_length * radius_slope)),
                                         radius2=radius_base - ((i+1) * (link_length * radius_slope)),
                                         vertices=8,
-                                        depth=link_length,
-                                        location=prev_end_location,
-                                        rotation=rotation)
+                                        depth=link_length,)
+                                        # location=prev_end_location,
+                                        # rotation=rotation)
         segment = bpy.context.object
         segment.name = f"Whisker_{whisker_name}_Segment_{i}"
         
-        # Parenting
-        if parent_obj:
-            segment.parent = parent_obj
-#            segment.location = (0, 0, link_length)
+        segment.matrix_world = total_transform
         
-        parent_obj = segment
+        # Parenting
+        # if parent_obj:
+        #     segment.parent = parent_obj
+        #     segment.location = (0, 0, link_length)
+        
+        # parent_obj = segment
+        link_prev = segment.matrix_world().copy()
 
 
 if __name__ == "__main__":
-    whisker_names, whisker_geom, whisker_angles, whisker_pos, base_pos, base_rot = data_reader()
-    for i in range(len(whisker_names)//2):
+    whisker_names, whisker_geom, whisker_angles,\
+    whisker_pos, whisker_bp_coor, whisker_bp_angles = data_reader()
+    for i in range(len(whisker_names)):
         ##############################
         # configurations for whiskers
         ##############################
@@ -147,11 +177,9 @@ if __name__ == "__main__":
         radius_tip = radius_base - whisker_length*radius_slope        
         link_angles = whisker_angles[i]
         NUM_LINKS = len(link_angles)
-        location=(base_pos[i][0], base_pos[i][1], base_pos[i][2]) #(i*2,0,0)
-        rotation=(base_rot[i][0]-math.pi/2, -base_rot[i][0], base_rot[i][0]+math.pi/2)
+        base_pos=(whisker_bp_coor[i][0], whisker_bp_coor[i][1], whisker_bp_coor[i][2]) #(i*2,0,0)
+        base_rot=(whisker_bp_angles[i][0]-math.pi/2, -whisker_bp_angles[i][0], whisker_bp_angles[i][0]+math.pi/2)
         ##############################
-        # same applies to the location
-        # I am not worrying about the angles between links for now
         create_whisker_segment(whisker_name=whisker_name,
                                side=side,
                                row=row,
@@ -161,6 +189,6 @@ if __name__ == "__main__":
                                radius_slope=radius_slope,
                                radius_tip=radius_tip,
                                link_angles=link_angles,
-                               location=location,
-                               rotation=rotation,
+                               base_pos=base_pos,
+                               base_rot=base_rot,
                                NUM_LINKS=NUM_LINKS)
